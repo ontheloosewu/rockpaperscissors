ba028a9b9760dc38e12bb2952bde803e33382cfe
Timothy Wu <twu23@buffalo.edu>
2022-03-09 02:32:44 -0500

    Play round function implemented, no "choice" yet..

--- rps.js
+++ rps.js
@@ -1,4 +1,5 @@
 function computerPlay() {
+    // 3 choices
     let choice = Math.floor(Math.random() * 3);
 
     if (choice === 0) {
@@ -13,7 +14,49 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    //take any case sensitive input
+    playerSelection = playerSelection.toLowerCase();
     
+    //to see what selections are
+    //console.log("Player chose: " + playerSelection);
+    //console.log("Computer chose: " + computerSelection);
+
+    // if player selected "choice", go through computer choices
+    if (playerSelection === "rock") {
+        if (computerSelection === "Paper") {
+            return "You Lose! Paper beats Rock";
+        }
+        else if (computerSelection === "Scissors") {
+            return "You Win! Rock beats Scissors";
+        }
+        else {
+            return "It's a tie!";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "Rock") {
+            return "You Win! Paper beats Rock";
+        }
+        else if (computerSelection === "Scissors") {
+            return "You Lose! Scissors beats Paper";
+        }
+        else {
+            return "It's a tie!";
+        }
+    }
+    // player selected scissors
+    else {
+        if (computerSelection === "Paper") {
+            return "You Win! Scissors beats Paper";
+        }
+        else if (computerSelection === "Rock") {
+            return "You Lose! Rock beats Scissors";
+        }
+        else {
+            return "It's a tie!";
+        }
+    }
 }
 
-console.log( computerPlay() );
+const playerSelection = "RoCk";
+console.log(playRound(playerSelection, computerPlay()));
